8f778211e215648d0c29a844b95bf805f127e626
Ymarcos1984 <ymarcosroque@gmail.com>
2026-07-13 13:20:25 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'mapping-est-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }).then(function(){ return self.skipWaiting(); }));
+});
+self.addEventListener('activate', function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.map(function(k){ if (k !== CACHE) return caches.delete(k); }));
+  }).then(function(){ return self.clients.claim(); }));
+});
+self.addEventListener('fetch', function(e){
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached){
+      return cached || fetch(e.request).then(function(resp){
+        return caches.open(CACHE).then(function(c){ try{ c.put(e.request, resp.clone()); }catch(_){} return resp; });
+      }).catch(function(){ return caches.match('./index.html'); });
+    })
+  );
+});
